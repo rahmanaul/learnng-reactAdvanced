3f37c9065b00f7a342d09ef6b26f9ecd19215bce
rahmanaul <rahmannauliaaa@gmail.com>
2020-11-14 13:36:20 +0700

    UseState - Array

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import Setup from './tutorial/1-useState/setup/3-useState-array';
 function App() {
   return (
     <div className='container'>
-      <h2>Advanced Tutorial</h2>
+      <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
